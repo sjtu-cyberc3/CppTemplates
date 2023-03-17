31d41c447332e8bfd6c7319b2830f4a49e11190b
xinyang <41963211+xinyang-go@users.noreply.github.com>
2023-03-18 00:30:50 +0800

    common::configs to reflection

--- ros/ros_example.cc
+++ ros/ros_example.cc
@@ -1,10 +1,10 @@
-// app
+// cpptemplates
 #include "app/app_example.h"
-// user
-#include "common/configs.hpp"
 #include "cpptemplates/DParamConfig.h"
 #include "cpptemplates/demo.h"
 #include "cpptemplates/func.h"
+#include "reflcpp/core.hpp"
+#include "reflcpp/yaml.hpp"
 #include "tools/rosbag.h"
 // ros
 #include <dynamic_reconfigure/server.h>
@@ -45,20 +45,42 @@ void dcfg_callback(cpptemplates::DParamConfig& cfg, uint32_t /*level*/) {
   app->feed_dcfg(cfg.int_param, cfg.double_param, cfg.str_param);
 }
 
+/****************************************/
+/*        program configs struct        */
+/****************************************/
+struct ProgramConfigs {
+  // rosbag
+  std::string bag_file;
+  float bag_rate;
+  // app configuration file path
+  std::string app_config;
+};
+REFLCPP_METAINFO(ProgramConfigs, , (bag_file)(bag_rate))
+REFLCPP_YAML(ProgramConfigs)
+
+void as_absolute_path(std::string &path, const std::string &prefix) {
+  if(fs::path(path).is_relative()) {
+    path = fs::path(prefix) / path;
+  }
+}
+
 int main(int argc, char* argv[]) {
   /****************************************/
   /*      configure from command line     */
   /****************************************/
-  std::string  config_file_path = fs::path(WORK_SPACE_PATH) / "config/ros_example.yaml";
-  ConfigLoader cfg;
-  ConfigDef(cfg, std::string, bag_file);
-  ConfigDef(cfg, std::string, AppExampleConfig);
-  cfg.open(config_file_path);
-  cfg.load_once();
+  if (argc != 2) {
+    ROS_FATAL_STREAM("usage: " << argv[0] << " CONFIG_FILE.");
+    return -1;
+  }
+  ROS_INFO_STREAM("configuring from " << argv[1]);
+  auto prog_cfg = YAML::LoadFile(argv[1]).as<ProgramConfigs>();
+  auto prefix_path = fs::path(argv[1]).parent_path();
+  as_absolute_path(prog_cfg.bag_file, prefix_path);
+  as_absolute_path(prog_cfg.app_config, prefix_path);
   /****************************************/
   /*            configure app             */
   /****************************************/
-  app = std::make_unique<AppExample>(AppExampleConfig);
+  app = std::make_unique<AppExample>(prog_cfg.app_config);
 
   /****************************************/
   /*            configure ros             */
@@ -84,10 +106,10 @@ int main(int argc, char* argv[]) {
   /*            configure bag             */
   /****************************************/
   BagPlayer bag_player(nh);
-  if (!bag_file.empty()) {
-    bag_player.open(bag_file);
+  if (!prog_cfg.bag_file.empty()) {
+    bag_player.open(prog_cfg.bag_file);
     bag_player.set_queue_size(1);
-    bag_player.set_rate(1);  // set to 0 for full speed (not recommend)
+    bag_player.set_rate(prog_cfg.bag_rate);  // set to 0 for full speed (not recommend)
   }
 
   /****************************************/
